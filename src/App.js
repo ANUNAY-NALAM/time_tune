4574832e820c116a7bca1b45a3799343c99b03bf
ANUNAY-NALAM <anunaynalam@gmail.com>
2023-08-17 00:54:14 +0530

    register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {Dashboard,Register,Landing,Error} from './pages'
 function App() {
   return (
-    <div className="App">
-      react
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to='/'>Dashboard</Link>
+        <Link to='/register'>Register</Link>
+        <Link to='/landing'>Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App;
